a840fc3e525c0e00862eafb94583d80ae3206046
Glen Malone <glenmalone95@gmail.com>
2018-07-27 17:33:05 +0100

    init
    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	[SerializeField] private float distanceToMove;
+	[SerializeField] private float moveSpeed;
+	private bool moveToPoint = false;
+	private Vector3 endPosition;
+	void Start () {
+    	endPosition = transform.position;
+    }
+  
+	void FixedUpdate () {
+    	if (moveToPoint)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, endPosition, moveSpeed * Time.deltaTime);
+        }
+    }
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) //Left Press
+        {
+            endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+            moveToPoint = true;
+        }
+        if (Input.GetKeyUp(KeyCode.A)) //Left Release
+        {
+            endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+            moveToPoint = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D)) //Right Press
+        {
+            endPosition = new Vector3(endPosition.x + distanceToMove, endPosition.y, endPosition.z);
+            moveToPoint = true;
+        }
+        if (Input.GetKeyUp(KeyCode.D)) //Right Release
+        {
+            endPosition = new Vector3(endPosition.x - distanceToMove, endPosition.y, endPosition.z);
+            moveToPoint = true;
+        }
+        // if (Input.GetKeyDown(KeyCode.W)) //Up
+        // {
+        //     endPosition = new Vector3(endPosition.x, endPosition.y + distanceToMove, endPosition.z);
+        //     moveToPoint = true;
+        // }
+        // if (Input.GetKeyDown(KeyCode.S)) //Down
+        // {
+        //     endPosition = new Vector3(endPosition.x, endPosition.y - distanceToMove, endPosition.z);
+        //     moveToPoint = true;
+        // }
+    }
+}
