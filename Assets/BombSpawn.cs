a4fa206000bcd69f7e69e1e9e52b9766ed6807d2
Glen Malone <glenmalone95@gmail.com>
2018-07-30 17:20:35 +0100

    Added bomb spawning

---
+++ Assets/BombSpawn.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombSpawn : MonoBehaviour {
+
+    public float Timer = .5f;
+    public GameObject bomb;
+    GameObject bombClone;
+ 
+    void Start () 
+    {
+
+    }
+     
+    void Update () 
+    {
+        Timer -= Time.deltaTime;
+        if (Timer <= 0){
+        	bombClone = Instantiate (bomb,(new Vector3 ((Random.Range (-1, 2)), 2, 0)), transform.rotation )  as GameObject;
+            Timer = .5f;
+        }
+    }
+}
